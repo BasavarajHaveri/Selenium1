cc022286453883e580f4bcf107924f2f54e0bd8e
unknown <mindedge96@iiht.tech>
2022-07-04 17:31:06 +0530

    java file

--- selenium1.java
+++ selenium1.java
@@ -1,7 +1,10 @@
 package com.mindtree.seleniumclass;
 
+import java.sql.Driver;
+
 import javax.sql.rowset.WebRowSet;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebDriverInfo;
@@ -12,9 +15,12 @@ public class selenium1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exc");
+      System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
       WebDriver driver=new ChromeDriver();
-      driver.get("https://www.google.com/");
+      driver.get("https://www.moneycontrol.com/");
+      driver .manage() .window().maximize();
+      driver .findElement(By.id("twotabsearchtextbox")).sendKeys("personal Finance");
+      
 	}
 
 }
